2847615172cd67115fe5b2bcdb513aa0c4042c83
METE <metehanalper03@gmail.com>
2021-04-05 18:59:28 +0300

    5.04.2021 18.58 sign in sign up tasarımı yapıldı

---
+++ app/src/main/java/com/bank/izbank/sign_up.java
@@ -0,0 +1,14 @@
+package com.bank.izbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
