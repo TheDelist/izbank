4c84aafaa7e38a14e0d6c690d39fc3908170df08
METE <metehanalper03@gmail.com>
2021-04-05 18:08:15 +0300

    m3tee

---
+++ app/src/main/java/com/bank/izbank/Deneme.java
@@ -0,0 +1,5 @@
+package com.bank.izbank;
+
+public class Deneme {
+    //mete deneme mete
+}
--- app/src/main/java/com/bank/izbank/MainActivity.java
+++ app/src/main/java/com/bank/izbank/MainActivity.java
@@ -20,5 +20,6 @@ public class MainActivity extends AppCompatActivity {
     //naber lannn
 
     //halil yazdııı
+    //sadece commit deneme
 
 }
