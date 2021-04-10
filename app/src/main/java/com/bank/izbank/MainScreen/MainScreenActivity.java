6a8f730e3f8ae70cdc3e1b53d85afeb980ea68ef
Halil Akpınar <halilakpinar4115@gmail.com>
2021-04-10 21:32:00 +0300

    10.04.2021  21.31  fragment finance hariç final

--- app/src/main/java/com/bank/izbank/MainScreen/MainScreenActivity.java
+++ app/src/main/java/com/bank/izbank/MainScreen/MainScreenActivity.java
@@ -3,6 +3,7 @@ package com.bank.izbank.MainScreen;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -40,9 +41,15 @@ public class MainScreenActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
 
+    final Fragment fragment1 = new Fragment1();
+    final Fragment fragment2 = new Fragment2();
+    final Fragment fragment4 = new Fragment4();
+    final Fragment fragment5 = new Fragment5();
 
+    private Fragment tempFragment=fragment1;
 
-    private Fragment tempFragment;
+    final FragmentManager fm = getSupportFragmentManager();
+
     private ArrayList<CryptoModel> cryptoModels;
     private final String BASE_URL = "https://api.nomics.com/v1/";
     private Retrofit retrofit;
@@ -66,32 +73,41 @@ public class MainScreenActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
 
+        fm.beginTransaction().add(R.id.fragment_container,fragment5,"5").hide(fragment5).commit();
+        fm.beginTransaction().add(R.id.fragment_container,fragment4,"4").hide(fragment4).commit();
+        fm.beginTransaction().add(R.id.fragment_container,fragment2,"2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.fragment_container,fragment1,"1").commit();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,new Fragment1()).commit();
+
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 switch (item.getItemId()){
                     case R.id.menu1:
-                        tempFragment=new Fragment1();
+                        fm.beginTransaction().hide(tempFragment).show(fragment1).commit();
+                        tempFragment = fragment1;
                         break;
                     case R.id.menu2:
-                        tempFragment=new Fragment2();
+                        fm.beginTransaction().hide(tempFragment).show(fragment2).commit();
+                        tempFragment = fragment2;
                         break;
                     case R.id.menu3:
-                        tempFragment= new FinanceFragment(cryptoModels);
+                        getSupportFragmentManager().beginTransaction().hide(tempFragment).add(R.id.fragment_container,tempFragment=new FinanceFragment(cryptoModels)).show(tempFragment).commit();
                         break;
                     case R.id.menu4:
-                        tempFragment=new Fragment4();
+                        fm.beginTransaction().hide(tempFragment).show(fragment4).commit();
+                        tempFragment = fragment4;
                         break;
                     case R.id.menu5:
-                        tempFragment=new Fragment5();
+                        fm.beginTransaction().hide(tempFragment).show(fragment5).commit();
+                        tempFragment = fragment5;
                         break;
 
                 }
 
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,tempFragment).commit();
+
                 return true;
             }
         });
