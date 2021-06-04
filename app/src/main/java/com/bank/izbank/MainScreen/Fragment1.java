3d3117c19a8b6629bba31d66a3ca3f7e8f3f4019
Metehan Alper Elmas <79103082+metehanalper@users.noreply.github.com>
2021-06-04 21:38:58 +0300

    fragment 1 hesap ekleme tuşları aktifleştirildi

--- app/src/main/java/com/bank/izbank/MainScreen/Fragment1.java
+++ app/src/main/java/com/bank/izbank/MainScreen/Fragment1.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,7 +27,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Fragment1 extends Fragment {
-
+    ImageView add_bank_account, add_credit_card;
     RecyclerView recyclerView;
     RecyclerView recyclerViewbankaccount;
     TextView text_view_name, date;
@@ -43,9 +44,11 @@ public class Fragment1 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        myCreditCard = new ArrayList<>();
+        myBankAccount = new ArrayList<>();
         define();
         setDate();
+        click();
 
         text_view_name.setText("HELLO, " + SignIn.mainUser.getName().toUpperCase()+".");
 
@@ -57,22 +60,8 @@ public class Fragment1 extends Fragment {
         recyclerViewbankaccount.setLayoutManager(new LinearLayoutManager(getActivity()));
 
 
-        myCreditCard = new ArrayList<>();
-        myCreditCard.add(new CreditCard(2000 ));
-        myCreditCard.add(new CreditCard(2000 ));
-        myCreditCard.add(new CreditCard(2000 ));
-        myCreditCard.add(new CreditCard(2000 ));
-        myCreditCard.add(new CreditCard(2000 ));
-        myCreditCard.add(new CreditCard(2000 ));
-        myCreditCard.add(new CreditCard(2000 ));
 
 
-        myBankAccount = new ArrayList<>();
-        myBankAccount.add(new BankAccount(1234));
-        myBankAccount.add(new BankAccount(1234));
-        myBankAccount.add(new BankAccount(1234));
-        myBankAccount.add(new BankAccount(1234));
-        myBankAccount.add(new BankAccount(1234));
 
 
         MyCreditCardAdapter myCreditCardAdapter = new MyCreditCardAdapter(myCreditCard,getActivity() );
@@ -90,8 +79,29 @@ public class Fragment1 extends Fragment {
         date = getView().findViewById(R.id.text_view_date_main);
         recyclerView = getView().findViewById(R.id.recyclerview_credit_card);
         recyclerViewbankaccount = getView().findViewById(R.id.recyclerview_bank_account);
+        add_bank_account = getView().findViewById(R.id.image_view_add_bank_account);
+        add_credit_card = getView().findViewById(R.id.image_view_add_credit_card);
 
 
+    }
+
+    public void click(){
+        add_bank_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myBankAccount.add(new BankAccount(1234));
+                MyBankAccountAdapter myBankAccountAdapter = new MyBankAccountAdapter(myBankAccount,getActivity() );
+                recyclerViewbankaccount.setAdapter(myBankAccountAdapter);
+            }
+        });
+        add_credit_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myCreditCard.add(new CreditCard(9999));
+                MyCreditCardAdapter myCreditCardAdapter = new MyCreditCardAdapter(myCreditCard,getActivity() );
+                recyclerView.setAdapter(myCreditCardAdapter);
+            }
+        });
     }
 
     public void setDate(){
