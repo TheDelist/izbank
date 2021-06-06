b3182fcec3a89ac21fd31adff4c73f646fee8589
samet kemal özdemir <samet.ozdemir@ceng.deu.edu.tr>
2021-06-06 18:38:01 +0300

    06.06.2021 18.37 finance screen search oldu .

--- app/src/main/java/com/bank/izbank/MainScreen/FinanceScreen/FinanceFragment.java
+++ app/src/main/java/com/bank/izbank/MainScreen/FinanceScreen/FinanceFragment.java
@@ -8,19 +8,28 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bank.izbank.Adapters.CryptoPostAdapter;
+import com.bank.izbank.Bill.Bill;
+import com.bank.izbank.Bill.BillAdapter;
 import com.bank.izbank.R;
 import com.bank.izbank.Sign.SignIn;
 
@@ -28,12 +37,14 @@ import java.util.ArrayList;
 
 import static com.parse.Parse.getApplicationContext;
 
-public class FinanceFragment extends Fragment {
+public class FinanceFragment extends Fragment implements SearchView.OnQueryTextListener {
         //exchange https://nomics.com/docs/#tag/Exchange-Rates
         //https://api.nomics.com/v1/exchange-rates?key=c5d0683b83dc6e2dbd00841b72f7c86c
-    private ArrayList<CryptoModel> cryptoModels;
+    private ArrayList<CryptoModel> cryptoModels, searchList;
     private RecyclerView recyclerView;
-    private CryptoPostAdapter cryptoPostAdapter;
+    private CryptoPostAdapter cryptoPostAdapter ,searchAdapter;
+    private ItemClickListener itemClickListener;
+    private Toolbar toolbar;
 
     public FinanceFragment(ArrayList<CryptoModel> list){
         this.cryptoModels=list;
@@ -52,12 +63,16 @@ public class FinanceFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        toolbar = getView().findViewById(R.id.toolbar);
+        toolbar.setTitle("Search ");
+        //toolbar.setLogo(R.drawable.icon_bill);
 
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
 
         recyclerView=view.findViewById(R.id.recyclerView_finance);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        cryptoPostAdapter = new CryptoPostAdapter(cryptoModels, getActivity(), getContext(), new ItemClickListener() {
+        itemClickListener=new ItemClickListener() {
             @Override
             public void onItemClicked(RecyclerView.ViewHolder vh, CryptoModel item, int pos) {
                 //popup menu
@@ -65,8 +80,8 @@ public class FinanceFragment extends Fragment {
 
                 AlertDialog.Builder buyCryptoPopup=new AlertDialog.Builder(getContext());
                 buyCryptoPopup.setTitle("Crypto Currency Buy");
-               Drawable d = item.getImage().getDrawable();
-               buyCryptoPopup.setIcon(d);
+                Drawable d = item.getImage().getDrawable();
+                buyCryptoPopup.setIcon(d);
 
                 buyCryptoPopup.setView(R.layout.finance_screen_cryto_buy_popup);
                 LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -115,13 +130,71 @@ public class FinanceFragment extends Fragment {
                     }
                 });
                 buyCryptoPopup.create().show();
-             //   Toast.makeText(getActivity(), "Item clicked: " + pos+" item"+item.getCurrencyName(), Toast.LENGTH_SHORT).show();
+                //   Toast.makeText(getActivity(), "Item clicked: " + pos+" item"+item.getCurrencyName(), Toast.LENGTH_SHORT).show();
             }
-        });
+        };
+
+        cryptoPostAdapter = new CryptoPostAdapter(cryptoModels, getActivity(), getContext(), itemClickListener);
+
         recyclerView.setAdapter(cryptoPostAdapter);
         cryptoPostAdapter.notifyDataSetChanged();
 
 
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+       inflater.inflate(R.menu.crypto_search,menu);
+        MenuItem menuItem=menu.findItem(R.id.search_view);
+        SearchView searchView=(SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+    }
+    public void toolbarSearch(String searchWord){
+
+         searchList = searchBill(cryptoModels, searchWord);
+
+        searchAdapter = new CryptoPostAdapter(searchList,getActivity(),getContext(),itemClickListener);
+        recyclerView.setAdapter(searchAdapter);
+
+    }
+
+    public ArrayList<CryptoModel> searchBill(ArrayList<CryptoModel> list, String string){
+
+        ArrayList<CryptoModel> returnList = new ArrayList<>();
+
+        if(string.equals("")){
+            return list;
+        }
+        else {
+            for(CryptoModel cryptoModel: list){
+
+
+
+                if( cryptoModel.getCurrencyName().toLowerCase().contains(string.toLowerCase())
+                        || String.valueOf(cryptoModel.getPrice()).toLowerCase().contains(string.toLowerCase()) || cryptoModel.getCurrencySymbol().toLowerCase().contains(string.toLowerCase())){
+                    returnList.add(cryptoModel);
+                }
+
+            }
+            return returnList;
+        }
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        toolbarSearch(query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        toolbarSearch(newText);
+        return false;
+    }
 }
